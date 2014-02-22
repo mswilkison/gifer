61a2203123172c85c26a15af34c65e74fc99120a
MacLane Wilkison <mwilkison@gmail.com>
2014-02-22 10:13:39 -0800

    add gallery helpers to util

--- src/gifer/util.clj
+++ src/gifer/util.clj
@@ -1,6 +1,17 @@
 (ns gifer.util
-  (:require [noir.io :as io]
-            [markdown.core :as md]))
+  (:require [noir.session :as session]
+            [ring.util.codec :refer [url-encode]]
+            [noir.io :as io]
+            [markdown.core :as md])
+  (:import java.io.File))
+
+(def galleries "galleries")
+
+(defn gallery-path []
+  (str galleries File/separator (session/get :user-id)))
+
+(defn image-uri [userid file-name]
+  (str "/img/" userid "/" (url-encode file-name)))
 
 (defn md->html
   "reads a markdown file from public/md and returns an HTML string"
