9416537435d65e85633fa1c7d9f6b60e6f50f9fa
MacLane Wilkison <mwilkison@gmail.com>
2014-02-22 15:38:03 -0800

    pass blob and effect to save router

--- src/gifer/routes/save.clj
+++ src/gifer/routes/save.clj
@@ -2,13 +2,16 @@
   (:use compojure.core)
   (:require [gifer.views.layout :as layout]
             [clojure.java.io :as io]
+            [ring.util.codec :refer [base64-decode]]
+            [noir.io :refer [upload-file]]
             [noir.session :as session]
             [noir.response :as resp]
             [noir.util.route :refer [restricted]]
-            [gifer.util :as util]
+            [gifer.util :refer [gallery-path]]
             [gifer.models.db :as db]))
 
-(defn handle-upload [outVideo effect])
+(defn handle-upload [blob effect]
+  (resp/redirect "/"))
 
 (defroutes save-routes
-  (POST "/save" [outVideo effect] (handle-upload outVideo effect)))
+  (POST "/save" [blob effect] (handle-upload blob effect)))
